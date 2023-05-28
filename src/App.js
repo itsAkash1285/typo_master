04c781bb58507efcab4c99c276304f8694b565c3
Akash Patil <patilakash1285@gmail.com>
2023-05-28 20:37:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./css/App.css";
+import MainKeyBoard from "./keyboard/MainKeyBoard";
+import { Provider } from "react-redux";
+import NavbarComponent from "./layouts/NavbarComponent";
+import TypingComponent from "./typing/TypingComponent";
+import Dashboard from "./summary/Dashboard";
+import Sounds from "./sounds/Sounds";
+import SummaryComponent from "./summary/SummaryComponent";
+import store from "./store";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <div className="container d-flex">
+                    <Sounds />
+                    <div className="flex-8 jcc ">
+                        <NavbarComponent />
+                        <div className="bg-white flex-8">
+                            <TypingComponent />
+                            <Dashboard />
+                            <MainKeyBoard />
+                        </div>
+                    </div>
+                    <div className="flex-4">
+                        <SummaryComponent />
+                    </div>
+                </div>
+            </Provider>
+        );
+    }
 }
 
 export default App;
